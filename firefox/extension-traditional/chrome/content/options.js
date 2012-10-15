26120ef75d1ebba1108e2e6fd24f606b35d93aad
Christopher Finke <cfinke@gmail.com>
2012-10-15 15:56:30 +0000

    Moved options functions into a COMMENT_SNOB_OPTIONS namespace.

--- firefox/extension-traditional/chrome/content/options.js
+++ firefox/extension-traditional/chrome/content/options.js
@@ -2,183 +2,185 @@ Components.utils.import("resource://comment-snob-modules/comment-snob-minify.js"
 Components.utils.import("resource://comment-snob-modules/comment-snob-util.js");
 Components.utils.import("resource://comment-snob-modules/comment-snob-updater.js");
 
-var currentRuleID = null;
-
-function populateRuleList() {
-	$( "#navbar-container label.user-rule" ).remove();
+var COMMENT_SNOB_OPTIONS = {
+	currentRuleID : null,
 	
-	var rules = COMMENT_SNOB_UTIL.getJSONPref("rules", {});
+	populateRuleList : function () {
+		$( "#navbar-container label.user-rule" ).remove();
 	
-	for (var i in rules) {
-		var option = $( '<label/>' );
-		option.addClass("navbar-item user-rule");
-		option.attr("ruleid", i);
-		option.text(rules[i].label);
-		option.attr("pagename", "rule");
-		option.val(i);
+		var rules = COMMENT_SNOB_UTIL.getJSONPref("rules", {});
+	
+		for (var i in rules) {
+			var option = $( '<label/>' );
+			option.addClass("navbar-item user-rule");
+			option.attr("ruleid", i);
+			option.text(rules[i].label);
+			option.attr("pagename", "rule");
+			option.val(i);
 		
-		$( '#default-rule' ).after( option );
-	}
+			$( '#default-rule' ).after( option );
+		}
 	
-	if (!("youtube@chrisfinke.com" in rules)) {
-		$("#reinstall-youtube").show();
-	}
-	else {
-		$("#reinstall-youtube").hide();
-	}
-	
-	$( "#default-rule" ).click();
-}
-
-function showRuleSettings(ruleId) {
-	var prefs = COMMENT_SNOB_UTIL.getJSONPref("rulePrefs", {});
-	
-	var customPrefs = false;
-	
-	if (ruleId in prefs) {
-		var rulePrefs = prefs[ruleId];
-		customPrefs = true;
-	}
-	else {
-		var rulePrefs = COMMENT_SNOB_UTIL.defaultPrefs;
-	}
-	
-	$(".preference-bool").each(function () {
-		this.checked = rulePrefs[$(this).attr("pref")];
-	});
-	
-	$(".preference-int").each(function () {
-		$(this).val(parseInt(rulePrefs[$(this).attr("pref")], 10));
-	});
-
-	$(".preference-text").each(function () {
-		$(this).val(rulePrefs[$(this).attr("pref")]);
-	});
-	
-	$("#custom-preferences").css("visibility", "visible");
-	
-	setDisabled();
-	
-	if (ruleId) {
-		currentRuleID = ruleId;
-		$("#rule-management").show();
-		$("#default-preferences").show();
-		
-		if (!customPrefs) {
-			$("#use-default").each(function () { this.checked = true; });
+		if (!("youtube@chrisfinke.com" in rules)) {
+			$("#reinstall-youtube").show();
 		}
 		else {
-			$("#use-default").each(function () { this.checked = false; });
+			$("#reinstall-youtube").hide();
 		}
-	}
-	else {
-		currentRuleID = null;
-		$("#rule-management").hide();
-		$("#default-preferences").hide();
-	}
 	
-	$( '#update-progress' ).text( '' );
-	
-	useDefaultChange();
-}
+		$( "#default-rule" ).click();
+	},
 
-function setDisabled() {
-	var disabled = $("#extreme").is(":checked");
-
-	$(".preference").each(function () {
-		if ($(this).attr("id") != "extreme") {
-			if ( disabled ) {
-				$(this).attr("disabled", true);
-			}
-			else {
-				$(this).removeAttr("disabled");
-			}
-		}
-	});
-}
-
-function save() {
-	if (!currentRuleID) {
-		$(".preference-bool").each(function () {
-			COMMENT_SNOB_UTIL.prefs.setBoolPref($(this).attr("pref"), $(this).is(":checked"));
-		});
-		
-		$(".preference-int").each(function () {
-			COMMENT_SNOB_UTIL.prefs.setIntPref($(this).attr("pref"), $(this).val());
-		});
-
-		$(".preference-text").each(function () {
-			COMMENT_SNOB_UTIL.prefs.setCharPref($(this).attr("pref"), $(this).val());
-		});
-	}
-	else {
-		var prefObject = {};
-
-		$(".preference-bool").each(function () {
-			prefObject[$(this).attr("pref")] = $(this).is(":checked");
-		});
-
-		$(".preference-int").each(function () {
-			prefObject[$(this).attr("pref")] = $(this).val();
-		});
-
-		$(".preference-text").each(function () {
-			prefObject[$(this).attr("pref")] = $(this).val();
-		});
-
+	showRuleSettings : function ( ruleId ) {
 		var prefs = COMMENT_SNOB_UTIL.getJSONPref("rulePrefs", {});
-		prefs[currentRuleID] = prefObject;
-
-		COMMENT_SNOB_UTIL.setJSONPref("rulePrefs", prefs);
-	}
-}
-
-function useDefaultChange() {
-	var useDefaultCheckbox = $('#use-default');
 	
-	if (currentRuleID && useDefaultCheckbox.is(':checked')) {
-		$("#custom-preferences").css("visibility", "hidden");
-		COMMENT_SNOB_UTIL.removeRulePrefs(currentRuleID);
-	}
-	else {
+		var customPrefs = false;
+	
+		if (ruleId in prefs) {
+			var rulePrefs = prefs[ruleId];
+			customPrefs = true;
+		}
+		else {
+			var rulePrefs = COMMENT_SNOB_UTIL.defaultPrefs;
+		}
+	
+		$(".preference-bool").each(function () {
+			this.checked = rulePrefs[$(this).attr("pref")];
+		});
+	
+		$(".preference-int").each(function () {
+			$(this).val(parseInt(rulePrefs[$(this).attr("pref")], 10));
+		});
+
+		$(".preference-text").each(function () {
+			$(this).val(rulePrefs[$(this).attr("pref")]);
+		});
+	
 		$("#custom-preferences").css("visibility", "visible");
 	
-		if (currentRuleID) {
-			save();
+		COMMENT_SNOB_OPTIONS.setDisabled();
+	
+		if (ruleId) {
+			COMMENT_SNOB_OPTIONS.currentRuleID = ruleId;
+			$("#rule-management").show();
+			$("#default-preferences").show();
+		
+			if (!customPrefs) {
+				$("#use-default").each(function () { this.checked = true; });
+			}
+			else {
+				$("#use-default").each(function () { this.checked = false; });
+			}
+		}
+		else {
+			COMMENT_SNOB_OPTIONS.currentRuleID = null;
+			$("#rule-management").hide();
+			$("#default-preferences").hide();
+		}
+	
+		$( '#update-progress' ).text( '' );
+	
+		COMMENT_SNOB_OPTIONS.useDefaultChange();
+	},
+
+	setDisabled : function () {
+		var disabled = $("#extreme").is(":checked");
+
+		$(".preference").each(function () {
+			if ($(this).attr("id") != "extreme") {
+				if ( disabled ) {
+					$(this).attr("disabled", true);
+				}
+				else {
+					$(this).removeAttr("disabled");
+				}
+			}
+		});
+	},
+
+	save : function () {
+		if (!COMMENT_SNOB_OPTIONS.currentRuleID) {
+			$(".preference-bool").each(function () {
+				COMMENT_SNOB_UTIL.prefs.setBoolPref($(this).attr("pref"), $(this).is(":checked"));
+			});
+		
+			$(".preference-int").each(function () {
+				COMMENT_SNOB_UTIL.prefs.setIntPref($(this).attr("pref"), $(this).val());
+			});
+
+			$(".preference-text").each(function () {
+				COMMENT_SNOB_UTIL.prefs.setCharPref($(this).attr("pref"), $(this).val());
+			});
+		}
+		else {
+			var prefObject = {};
+
+			$(".preference-bool").each(function () {
+				prefObject[$(this).attr("pref")] = $(this).is(":checked");
+			});
+
+			$(".preference-int").each(function () {
+				prefObject[$(this).attr("pref")] = $(this).val();
+			});
+
+			$(".preference-text").each(function () {
+				prefObject[$(this).attr("pref")] = $(this).val();
+			});
+
+			var prefs = COMMENT_SNOB_UTIL.getJSONPref("rulePrefs", {});
+			prefs[COMMENT_SNOB_OPTIONS.currentRuleID] = prefObject;
+
+			COMMENT_SNOB_UTIL.setJSONPref("rulePrefs", prefs);
+		}
+	},
+
+	useDefaultChange : function () {
+		var useDefaultCheckbox = $('#use-default');
+	
+		if (COMMENT_SNOB_OPTIONS.currentRuleID && useDefaultCheckbox.is(':checked')) {
+			$("#custom-preferences").css("visibility", "hidden");
+			COMMENT_SNOB_UTIL.removeRulePrefs(COMMENT_SNOB_OPTIONS.currentRuleID);
+		}
+		else {
+			$("#custom-preferences").css("visibility", "visible");
+	
+			if (COMMENT_SNOB_OPTIONS.currentRuleID) {
+				COMMENT_SNOB_OPTIONS.save();
+			}
 		}
 	}
-}
+};
 
 addEventListener( "load", function () {
 	removeEventListener( "load", arguments.callee, false );
-	
+
 	// Instant-apply any preference changes.
 	$(".preference").on( "click change blur keyup", function () {
-		save();
+		COMMENT_SNOB_OPTIONS.save();
 	});
 
-	$("#extreme").click(setDisabled);
+	$("#extreme").click( COMMENT_SNOB_OPTIONS.setDisabled );
 
 	$("#remove").click(function () {
 		if (confirm("Are you sure?")) {
-			COMMENT_SNOB_UTIL.removeRule( currentRuleID );
-			populateRuleList();
+			COMMENT_SNOB_UTIL.removeRule( COMMENT_SNOB_OPTIONS.currentRuleID );
+			COMMENT_SNOB_OPTIONS.populateRuleList();
 		}
 	});
-	
+
 	$("#update").click( function () {
 		var progressContainer = $( '#update-progress' );
 		progressContainer.text( '' );
-		
+	
 		progressContainer.append(
 			$( '<description/>' ).text( COMMENT_SNOB.strings.getString( 'label_updating' ) )
 		).show();
-		
+	
 		var rules = COMMENT_SNOB_UTIL.getJSONPref( "rules", {} );
+	
+		if ( COMMENT_SNOB_OPTIONS.currentRuleID in rules ) {
+			var currentRule = rules[ COMMENT_SNOB_OPTIONS.currentRuleID ];
 		
-		if ( currentRuleID in rules ) {
-			var currentRule = rules[ currentRuleID ];
-			
 			COMMENT_SNOB_UPDATER.updateRule( currentRule, function ( rv ) {
 				if ( rv.status ) {
 					if ( JSON.stringify( rv.rule ) == JSON.stringify( currentRule ) ) {
@@ -203,7 +205,7 @@ addEventListener( "load", function () {
 							$( '<description/>' ).text( COMMENT_SNOB.strings.getString( rv.msg ) )
 						);
 					}
-					
+				
 					if ( "msgDebug" in rv ) {
 						var debug = $( '<textbox/>' );
 						debug.attr( 'multiline', 'true' );
@@ -226,33 +228,33 @@ addEventListener( "load", function () {
 			);
 		}
 	});
-	
+
 	$("#add-finish").click(function () {
 		$("#rule-error").hide();
-		
+	
 		var rule = $("#add-rule").val();
-		
+	
 		var rv = COMMENT_SNOB_UTIL.addRule( rule );
-		
+	
 		if (!rv.status) {
 			$("#rule-error").text(rv.msg).show();
 		}
 		else {
-			populateRuleList();
+			COMMENT_SNOB_OPTIONS.populateRuleList();
 			$("#navbar-container label[ruleid='" + rv.rule.id + "']").click();
 			$("#add-rule").val("");
 		}
 	});
-	
+
 	$( "#navbar-container" ).on( "click", ".navbar-item", function () {
 		$(".navbar-item-selected").removeClass("navbar-item-selected");
 		$(this).addClass("navbar-item-selected");
 		$(".page").hide();
 		$("#" + $(this).attr("pagename") + "Page").show();
 	});
-	
+
 	$( "#navbar-container" ).on( "click", "#default-rule, .user-rule", function () {
-		showRuleSettings($(this).attr("ruleid"));
+		COMMENT_SNOB_OPTIONS.showRuleSettings($(this).attr("ruleid"));
 
 		if ($(this).attr("ruleid")) {
 			$("#remove").removeAttr("disabled");
@@ -261,20 +263,20 @@ addEventListener( "load", function () {
 			$("#remove").attr("disabled", true);
 		}
 	});
-	
+
 	$("#install-youtube").on( "click", function (e) {
 		e.preventDefault();
-		
+	
 		COMMENT_SNOB_UTIL.addRule(COMMENT_SNOB_UTIL.youtubeRule);
-		populateRuleList();
+		COMMENT_SNOB_OPTIONS.populateRuleList();
 		$(".user-rule[ruleid='youtube@chrisfinke.com']").click();
 	});
-	
+
 	$("#use-default").click(function () {
-		useDefaultChange();
+		COMMENT_SNOB_OPTIONS.useDefaultChange();
 	});
-	
-	populateRuleList();
-	
-	showRuleSettings();
+
+	COMMENT_SNOB_OPTIONS.populateRuleList();
+
+	COMMENT_SNOB_OPTIONS.showRuleSettings();
 }, false);
